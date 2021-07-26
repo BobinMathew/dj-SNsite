ae1f9d742250e3b3cde293f62d763b6b68a89696
Bobin <bobinm3@gmail.com>
2021-07-26 22:12:23 +0530

    created home page and about page

--- project/app1/views.py
+++ project/app1/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+def home(request):
     # return HttpResponse('Hello, World!')
     context = {'Name': 'Bobin'}
-    return render(request, 'app1/index.html', context)
+    return render(request, 'app1/home.html', context)
+
+def about(request):
+    context = {'Name': 'Bobin'}
+    return render(request, 'app1/about.html', context)
